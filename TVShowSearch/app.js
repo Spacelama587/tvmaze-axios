d86b76bab8f17590ac4512fbdb35f07ffeefd6ad
V <81617658+Spacelama587@users.noreply.github.com>
2023-12-23 01:44:37 +0530

    first commit

---
+++ TVShowSearch/app.js
@@ -0,0 +1,28 @@
+const form = document.querySelector("#searchForm");
+const imageContainer = document.querySelector("#imageContainer");
+
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const searchTerm = form.elements.query.value;
+  const config = { params: { q: searchTerm } };
+  const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+  clearImages();
+  makeImages(res.data);
+  form.elements.query.value = "";
+});
+
+const makeImages = (shows) => {
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement("IMG");
+      img.src = result.show.image.medium;
+      imageContainer.append(img);
+    }
+  }
+};
+
+const clearImages = () => {
+  while (imageContainer.firstChild) {
+    imageContainer.removeChild(imageContainer.firstChild);
+  }
+};
